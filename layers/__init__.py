dc75ebe755486c011c1bb51376d4263b3e4206a8
Дарина Мадуар <maduardar@yandex.ru>
2018-06-03 22:45:32 +0300

    Create __init__.py

---
+++ layers/__init__.py
@@ -0,0 +1,9 @@
+from .QuestionAttnGRU import QuestionAttnGRU
+from .SelfAttnGRU import SelfAttnGRU
+from .PointerGRU import PointerGRU
+from .QuestionPooling import QuestionPooling
+from .Argmax import Argmax
+from .Slice import Slice
+from .SharedWeight import SharedWeightLayer, SharedWeight
+from .VariationalDropout import VariationalDropout
+from .WrappedGRU import WrappedGRU
